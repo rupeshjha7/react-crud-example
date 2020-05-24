25db58b6b32417b7f37b2a38152dbe99408aa412
rupeshjha7 <jharupesh223@gmail.com>
2020-05-24 10:54:09 +0530

    update-file

--- src/app/showpost.js
+++ src/app/showpost.js
@@ -2,14 +2,14 @@ import React from "react";
 
 
 const ShowPost = (props) => {
-    const  {posts, onEdit, deletePost} = props;
+    const { posts, onEdit, deletePost } = props;
     return (
         <table className="table border shadow mt-4">
             <thead className="thead-dark">
                 <tr >
+                    <th scope="col">ID</th>
                     <th scope="col">TITLE</th>
                     <th scope="col">BODY</th>
-                    <th scope="col">USERID</th>
                     <th scope="col"></th>
                     <th scope="col"></th>
                 </tr>
@@ -17,9 +17,9 @@ const ShowPost = (props) => {
             {posts.map((post) => (
                 <tbody key={post.id}>
                     <tr >
+                        <td>{post.id}</td>
                         <td>{post.title}</td>
                         <td>{post.body}</td>
-                        <td>{post.userid}</td>
                         <td><button type="button" className="btn btn-primary"
                             onClick={() => onEdit(post)}>Update</button></td>
                         <td><button type="button" className="btn btn-primary"
